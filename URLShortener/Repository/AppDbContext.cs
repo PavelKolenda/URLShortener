2f36ba0a0a857bc9e019f81ffc73008c1cf96be6
PavelKolenda <progap0002@gmail.com>
2024-09-18 17:47:19 +0300

    Add DbContext

---
+++ URLShortener/Repository/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using URLShortener.Models;
+
+namespace URLShortener.Repository;
+public class AppDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    public AppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public DbSet<ShortenedUrl> ShortenedUrls { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 39));
+        options.UseMySql(_configuration.GetConnectionString("MySql"), serverVersion);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ShortenedUrl>(builder =>
+        {
+            builder.HasIndex(p => p.ShortUrl).IsUnique();
+        });
+    }
+}
+
