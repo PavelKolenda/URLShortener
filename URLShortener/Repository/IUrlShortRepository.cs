b186ccb7da3fb61bb8f5307c6809ade0bb1f7f54
PavelKolenda <progap0002@gmail.com>
2024-09-18 17:49:22 +0300

    Add UrlShortRepository

---
+++ URLShortener/Repository/IUrlShortRepository.cs
@@ -0,0 +1,9 @@
+﻿using URLShortener.Models;
+
+namespace URLShortener.Repository;
+public interface IUrlShortRepository
+{
+    Task AddAsync(ShortenedUrl shortenedUrl);
+    Task<ShortenedUrl> GetUrl(string shortUrl);
+    Task<bool> IsShortUrlUnique(string shortUrl);
+}
---
+++ URLShortener/Repository/UrlShortRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySqlConnector.Logging;
+using URLShortener.Models;
+
+namespace URLShortener.Repository;
+public class UrlShortRepository : IUrlShortRepository
+{
+    private readonly AppDbContext _context;
+
+    public UrlShortRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(ShortenedUrl shortenedUrl)
+    {
+        await _context.ShortenedUrls.AddAsync(shortenedUrl);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> IsShortUrlUnique(string shortUrl)
+    {
+        return !await _context.ShortenedUrls.AnyAsync(s => s.ShortUrl == shortUrl);
+    }
+
+    public async Task<ShortenedUrl> GetUrl(string shortCode)
+    {
+        var shortenedUrl = await _context.ShortenedUrls
+            .FirstOrDefaultAsync(s => s.ShortUrl.Substring(s.ShortUrl.Length - 6)
+            .Equals(shortCode)) 
+            ??
+            throw new Exception($"Short URL was not found.");
+
+        shortenedUrl.ClickCount++;
+        await _context.SaveChangesAsync();
+
+        return shortenedUrl;
+    } 
+}
