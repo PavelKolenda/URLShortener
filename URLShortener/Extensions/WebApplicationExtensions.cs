905db9ae951a90aaf78585e5463ff4eef95b46a1
PavelKolenda <progap0002@gmail.com>
2024-09-19 20:55:16 +0300

    Add WebApplicationExtensions

---
+++ URLShortener/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using URLShortener.Repository;
+
+namespace URLShortener.Extensions;
+public static class WebApplicationExtensions
+{
+    public static void ApplyMigrations(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        dbContext.Database.Migrate();
+    }
+}
