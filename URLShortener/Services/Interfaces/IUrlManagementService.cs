6c70f3011b35b0c1d1ff42690b683b3088edc5c4
PavelKolenda <progap0002@gmail.com>
2024-09-19 20:58:48 +0300

    Add UrlManagementService

---
+++ URLShortener/Services/Interfaces/IUrlManagementService.cs
@@ -0,0 +1,8 @@
+﻿using URLShortener.Models;
+
+namespace URLShortener.Services.Interfaces;
+public interface IUrlManagementService
+{
+    Task<ShortenedUrl> CreateShortUrlAsync(UrlShortenRequest request, HttpContext httpContext);
+    Task<string> GetLongUrlAsync(string shortUrl);
+}
---
+++ URLShortener/Services/UrlManagementService.cs
@@ -0,0 +1,44 @@
+﻿using URLShortener.Models;
+using URLShortener.Repository;
+using URLShortener.Services.Interfaces;
+
+namespace URLShortener.Services;
+
+public class UrlManagementService : IUrlManagementService
+{
+    private readonly IUrlShorteningService _urlShorteningService;
+    private readonly IUrlShortRepository _shortUrlRepository;
+    private readonly IUrlValidationService _urlValidationService;
+    public UrlManagementService(IUrlShorteningService urlShorteningService,
+                      IUrlShortRepository shortUrlRepository,
+                      IUrlValidationService urlValidationService)
+    {
+        _urlShorteningService = urlShorteningService;
+        _shortUrlRepository = shortUrlRepository;
+        _urlValidationService = urlValidationService;
+    }
+    public async Task<ShortenedUrl> CreateShortUrlAsync(UrlShortenRequest request, HttpContext httpContext)
+    {
+        _urlValidationService.ValidateUrl(request.LongUrl);
+
+        string shortUrl = await _urlShorteningService.Generate();
+
+        ShortenedUrl shortedUrl = new()
+        {
+            LongUrl = request.LongUrl,
+            ShortUrl = $"{httpContext.Request.Scheme}://{httpContext.Request.Host}/{shortUrl}",
+            CreatedAt = DateTime.Now
+        };
+
+        await _shortUrlRepository.AddAsync(shortedUrl);
+
+        return shortedUrl;
+    }
+
+    public async Task<string> GetLongUrlAsync(string shortUrl)
+    {
+        ShortenedUrl shortenedUrl = await _shortUrlRepository.GetUrlAsync(shortUrl);
+
+        return shortenedUrl.LongUrl;
+    }
+}
