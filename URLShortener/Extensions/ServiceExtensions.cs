bd89b735edab4e487ba42ad942a41f9d6799a118
PavelKolenda <progap0002@gmail.com>
2024-09-19 20:55:05 +0300

    Add ServiceExtensions

---
+++ URLShortener/Extensions/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using URLShortener.Extensions.ExceptionHandling;
+
+namespace URLShortener.Extensions;
+public static class ServiceExtensions
+{
+    public static void AddExceptionHandling(this IServiceCollection services)
+    {
+        services.AddExceptionHandler<InvalidUrlExceptionHandler>();
+        services.AddExceptionHandler<NotFoundExceptionHandler>();
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+    }
+}
+
