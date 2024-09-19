47f7a39bf50f8a507a01e9b0732abd734712f266
PavelKolenda <progap0002@gmail.com>
2024-09-19 20:59:16 +0300

    Update Progam.cs

--- URLShortener/Program.cs
+++ URLShortener/Program.cs
@@ -1,11 +1,29 @@
+using Microsoft.Extensions.Configuration;
+using URLShortener.Extensions;
+using URLShortener.Options;
+using URLShortener.Repository;
+using URLShortener.Services;
+using URLShortener.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddExceptionHandling();
+
+builder.Services.Configure<UrlShorteningOptions>(builder.Configuration.GetSection("UrlShorteningOptions"));
+
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped<IUrlService, UrlService>();
+builder.Services.AddScoped<IUrlShortRepository, UrlShortRepository>();
+builder.Services.AddScoped<IUrlManagementService, UrlManagementService>();
+builder.Services.AddScoped<IUrlValidationService, UrlValidationService>();
+builder.Services.AddScoped<IUrlShorteningService, UrlShorteningService>();
+
 
 var app = builder.Build();
 
@@ -14,9 +32,20 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ApplyMigrations();
 }
 
+app.UseExceptionHandler(opt => { });
+
 app.UseHttpsRedirection();
+
+app.UseCors(options =>
+{
+    options.SetIsOriginAllowed(origin => true)
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           .AllowCredentials();
+});
 
 app.UseAuthorization();
 
