bcdd8131dfa391e35ddd91cf6d08730bf1b330df
PavelKolenda <progap0002@gmail.com>
2024-09-19 20:57:45 +0300

    Add UrlService

---
+++ URLShortener/Services/Interfaces/IUrlService.cs
@@ -0,0 +1,10 @@
+﻿using URLShortener.DTO;
+using URLShortener.Models;
+
+namespace URLShortener.Services.Interfaces;
+public interface IUrlService
+{
+    Task DeleteAsync(int id);
+    Task<List<ShortenedUrl>> GetAsync();
+    Task UpdateAsync(int id, ShortenedUrlUpdateDto shortenedUrlDto);
+}
---
+++ URLShortener/Services/UrlService.cs
@@ -0,0 +1,38 @@
+﻿using URLShortener.DTO;
+using URLShortener.Models;
+using URLShortener.Repository;
+using URLShortener.Services.Interfaces;
+
+namespace URLShortener.Services;
+public class UrlService : IUrlService
+{
+    private readonly IUrlShortRepository _shortUrlRepository;
+    private readonly IUrlValidationService _urlValidationService;
+    public UrlService(IUrlShortRepository shortUrlRepository,
+                      IUrlValidationService urlValidationService)
+    {
+        _shortUrlRepository = shortUrlRepository;
+        _urlValidationService = urlValidationService;
+    }
+
+
+    public async Task<List<ShortenedUrl>> GetAsync()
+    {
+        return await _shortUrlRepository.GetAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        await _shortUrlRepository.DeleteAsync(id);
+    }
+
+    public async Task UpdateAsync(int id, ShortenedUrlUpdateDto shortenedUrlDto)
+    {
+        _urlValidationService.ValidateUrl(shortenedUrlDto.LongUrl);
+
+        var shortenedUrl = await _shortUrlRepository.GetByIdAsync(id);
+        shortenedUrl.LongUrl = shortenedUrlDto.LongUrl;
+
+        await _shortUrlRepository.UpdateAsync(shortenedUrl);
+    }
+}
