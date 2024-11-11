e6c61f1d64e7d7fbdf601f4753a8d37786e64b80
PavelKolenda <progap0002@gmail.com>
2024-11-11 19:32:21 +0300

    Add PagedParams

---
+++ URLShortener/Extensions/Pagination/PagedParams.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace URLShortener.Extensions.Pagination;
+
+public class PagedParams
+{
+    private int _pageSize;
+    public int Page { get; set; }
+    public int PageSize { 
+        get => _pageSize;
+        set
+        {
+            _pageSize = value switch
+            {
+                > 100 => 100,
+                <= 0 => 10,
+                _ => _pageSize
+            };
+        }
+    }
+}
