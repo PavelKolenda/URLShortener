bcf5535af40cdd828399f0292ddd390f4762620e
PavelKolenda <progap0002@gmail.com>
2024-11-11 19:33:28 +0300

    Add PagedList

---
+++ URLShortener/Extensions/Pagination/PagedList.cs
@@ -0,0 +1,17 @@
+namespace URLShortener.Extensions.Pagination;
+public class PagedList<T>
+{
+    public List<T> Items { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public bool HasPreviousPage => Page > 1;
+    public bool HasNextPage => Page * PageSize < TotalCount;
+    public PagedList(List<T> items, int page, int pageSize, int totalCount)
+    {
+        Items = items;
+        Page = page;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+    }
+}
